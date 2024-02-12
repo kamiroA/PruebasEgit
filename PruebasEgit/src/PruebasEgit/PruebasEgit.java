132b71cc7f4f12a53d78a5d561e169586e655b82
Camilo <ariesshow26@gmail.com>
2024-02-12 09:57:43 +0100

    Segundo Commit

--- PruebasEgit/src/PruebasEgit/PruebasEgit.java
+++ PruebasEgit/src/PruebasEgit/PruebasEgit.java
@@ -10,6 +10,8 @@ public class PruebasEgit {
 		System.out.println("Segundo commit máster");
 		
 		System.out.println("Primer commit rama 1");
+		
+		System.out.println("Segundo commit rama 1");
 
 	}
 
